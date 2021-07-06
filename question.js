2a361d4a12bf62381db7b336d44c6d9165df669e
viraj <viraj2272000@gmail.com>
2021-07-07 00:34:06 +0530

    2

---
+++ question.js
@@ -0,0 +1,28 @@
+console.log("hello");
+const quiz = [
+    {
+        q:'Which month comes right before june ?',
+        options:['may','april','december','july'],
+        answer :3
+    },
+    {
+        q:'What color is a banana ?',
+        options:['red','yellow','white','blue'],
+        answer :1
+    },
+    {
+        q:'3 + 5 = 8',
+        options:['True','False'],
+        answer :0
+    },
+    {
+        q:'In which year India won World Cup ?',
+        options:['2015','2003','1999','2011'],
+        answer :3
+    },
+    {
+        q:'2 ^ 4  ?',
+        options:['4','8','16','32'],
+        answer :2
+    }
+]
